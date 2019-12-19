ddc1262aa32682ab0ffb4d1ebe2c257d135f5575
Edward Cooke <edward.cooke@manchester.ac.uk>
2019-12-19 17:21:32 +0000

    [Add error catching]

--- sketch_dec03a/sketch_dec03a.ino
+++ sketch_dec03a/sketch_dec03a.ino
@@ -2,12 +2,17 @@ int up = 5;
 int down = 6;
 int a = 0;
 int b = 0;
-int c = 1;
+int c = 0;
+int del = 2;
+int error = 0;
 int PWMA = 3;
 int PWMB = 11;
 int DIRA = 12;
 int DIRB = 13;
+int endStop = 7;
+int dist = 0;
 void setup() {
+  pinMode(endStop, INPUT);
   pinMode(down, INPUT);
   pinMode(up, INPUT);
   pinMode(DIRA, OUTPUT);
@@ -18,54 +23,110 @@ void setup() {
 }
 
 void loop() {
-  
+  //Serial.println(dist);
   a = digitalRead(down);
   b = digitalRead(up);
-  if (a) {
-      digitalWrite(PWMA, HIGH);
-      digitalWrite(PWMB, HIGH);
-     
-      digitalWrite(DIRA, HIGH);      
-      digitalWrite(DIRB, HIGH);
-      delay(c);
-      
-      digitalWrite(DIRA, LOW);      
-      digitalWrite(DIRB, HIGH);
-      delay(c);
-      
-      digitalWrite(DIRA, LOW);      
-      digitalWrite(DIRB, LOW);
-      delay(c);
-      
-      digitalWrite(DIRA, HIGH);      
-      digitalWrite(DIRB, LOW);
-      delay(c);
-      
-      digitalWrite(PWMA, LOW);
-      digitalWrite(PWMB, LOW);
-      }
-if (b) {
-      digitalWrite(PWMA, HIGH);
-      digitalWrite(PWMB, HIGH);
-     
-      digitalWrite(DIRA, HIGH);      
-      digitalWrite(DIRB, HIGH);
-      delay(c);
-      
-      digitalWrite(DIRA, HIGH);      
-      digitalWrite(DIRB, LOW);
-      delay(c);
-      
-      digitalWrite(DIRA, LOW);      
-      digitalWrite(DIRB, LOW);
-      delay(c);
-      
-      digitalWrite(DIRA, LOW);      
-      digitalWrite(DIRB, HIGH);
-      delay(c);
-     
-      digitalWrite(PWMA, LOW);
-      digitalWrite(PWMB, LOW);
-      }
-}
+  c = digitalRead(endStop);
+  if(dist > 150){
+    error = 1;
+    a = 0;
+    b = 0;
+    Serial.print("Error ");
+    Serial.print(error);
+    Serial.println(": Too far out");
+  }
+  if(dist < 0){
+    error = 2;
+    a = 0;
+    b = 0;
+    Serial.print("Error ");
+    Serial.print(error);
+    Serial.println(": Negative reading");
+  }
+  if(!c){
+    error = 3;
+    a = 0;
+    b = 0;
+    Serial.print("Error ");
+    Serial.print(error);
+    Serial.println(": Endstop met");
+  }
 
+  if(error == 2 || error == 1){ //negative distance
+    Serial.println("Moving back in....");
+    while(c){
+      in();
+      c = digitalRead(endStop);
+    }
+    error = 3;
+  }
+  if(error == 3){ //met end stop
+    Serial.println("Moving back out....");
+    while(!c){    
+      out();      
+      c = digitalRead(endStop);
+    }
+    Serial.println("Endstop met");
+    dist = 0;
+    error = 0;
+    Serial.println("Distance reset.");
+  }
+  
+  if (a) {
+    out();
+    dist = dist + 1;
+    Serial.println(dist);
+  }
+  if (b) {
+    in();
+    dist = dist - 1;
+    Serial.println(dist);
+  }
+}
+void out(void){
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
+   
+  digitalWrite(DIRA, HIGH);      
+  digitalWrite(DIRB, HIGH);
+  delay(del);
+  
+  digitalWrite(DIRA, LOW);      
+  digitalWrite(DIRB, HIGH);
+  delay(del);
+  
+  digitalWrite(DIRA, LOW);      
+  digitalWrite(DIRB, LOW);
+  delay(del);
+  
+  digitalWrite(DIRA, HIGH);      
+  digitalWrite(DIRB, LOW);
+  delay(del);
+  
+  digitalWrite(PWMA, LOW);
+  digitalWrite(PWMB, LOW);
+}  
+
+void in(void){
+  digitalWrite(PWMA, HIGH);
+  digitalWrite(PWMB, HIGH);
+ 
+  digitalWrite(DIRA, HIGH);      
+  digitalWrite(DIRB, HIGH);
+  delay(del);
+  
+  digitalWrite(DIRA, HIGH);      
+  digitalWrite(DIRB, LOW);
+  delay(del);
+  
+  digitalWrite(DIRA, LOW);      
+  digitalWrite(DIRB, LOW);
+  delay(del);
+  
+  digitalWrite(DIRA, LOW);      
+  digitalWrite(DIRB, HIGH);
+  delay(del);
+ 
+  digitalWrite(PWMA, LOW);
+  digitalWrite(PWMB, LOW);
+}
